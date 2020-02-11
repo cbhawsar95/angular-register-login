2ba28f84c74c1f796d68997aac6590f545fc5fbf
Chanchal Bhawsar <bhawsar.chanchal95@gmail.com>
2020-02-12 03:25:13 +0530

    First commit for Angular App

---
+++ src/app/_models/user.ts
@@ -0,0 +1,11 @@
+﻿export class User {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  mobileno: string;
+  dob: Date;
+  cities: string;
+  notes: string;
+  token: string;
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,93 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+import { AlertService, UserService, AuthenticationService } from "../_services";
+import { of } from "rxjs";
+
+@Component({ templateUrl: "register.component.html" })
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  cities: { id: string; name: string }[];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(["/"]);
+    }
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")
+        ]
+      ],
+      mobileno: ["", [Validators.required, Validators.minLength(10)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      roomno: ["", [Validators.required, Validators.minLength(2)]],
+      dob: ["", Validators.required],
+      cities: ["", Validators.required],
+      notes: ["", Validators.required]
+    });
+    //Sync cities
+    // this.cities = this.getCities();
+    // this.registerForm.controls.cities.patchValue(this.cities[0].name);
+
+    // async orders
+    of(this.getCities()).subscribe(cities => {
+      this.cities = cities;
+      this.registerForm.controls.cities.patchValue(this.cities[0].name);
+    });
+  }
+
+  getCities() {
+    return [
+      { id: "1", name: "Indore" },
+      { id: "2", name: "Bhopal" },
+      { id: "3", name: "Pune" },
+      { id: "4", name: "Delhi" }
+    ];
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService
+      .register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success("Registration successful", true);
+          this.router.navigate(["/login"]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      );
+  }
+}
